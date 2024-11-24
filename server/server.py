bce926e06efe2ef00eb5bc9adc21aaa14d5cf3d6
Nikolaos Latzonis <nicklatzon@gmail.com>
2024-11-25 01:45:26 +0200

    Adding a simple server

--- server/server.py
+++ server/server.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Post the temperatures data from the incoming request and return the same data in the response
 @app.route('/temperatures', methods=['POST'])
 def post_temperatures():
     data = request.get_json()
-    return jsonify(data)
+    return jsonify({"data": data})
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
